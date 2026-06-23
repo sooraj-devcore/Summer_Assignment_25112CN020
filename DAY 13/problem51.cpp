26c844992d065bdbe0ae6deeb39de4a0f56c5a4e
Sooraj garg <surajtyagi@Surajs-MacBook-Air.local>
2026-06-23 09:43:20 +0530

    DAY 13 Uploaded

---
+++ DAY 13/problem51.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+
+    cin >> n;
+
+    int arr[n];
+
+    for(int i=0;i<n;i++)
+        cin >> arr[i];
+
+    int largest = arr[0];
+
+    int smallest = arr[0];
+
+    for(int i=1;i<n;i++) {
+
+        if(arr[i] > largest)
+            largest = arr[i];
+
+        if(arr[i] < smallest)
+            smallest = arr[i];
+    }
+
+    cout << largest << endl;
+
+    cout << smallest;
+
+    return 0;
+}
